7f03b6fe5ea15ed20f6a87054a49d3277631d66a
jiajieSP <56992742+jiajieSP@users.noreply.github.com>
2019-12-09 14:26:41 +0800

    TwitterUserCleanse Function

--- Social Media/Visual Studio/TwitterUserCleanse.py
+++ Social Media/Visual Studio/TwitterUserCleanse.py
@@ -9,68 +9,71 @@ import ast
 from googletrans import Translator
 from datetime import datetime
 
-datefile = input('Which data file do you want to cleanse ?')
+def UserCleanse():
+    datefile = input('Which data file do you want to cleanse ?')
 
-df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/CSV/Users/' + datefile + '.csv', names = ['User','ID','Date','Tweets'])
+    df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/CSV/Users/' + datefile + '.csv', names = ['User','ID','Date','Tweets'])
 
-#Extra rows by date 
-df['Date'] = pd.to_datetime(df['Date'])
+    #Extra rows by date 
+    df['Date'] = pd.to_datetime(df['Date'])
 
-startdate = '01-07-2018'
-enddate = '31-10-2019'
+    startdate = '01-07-2018'
+    enddate = '31-10-2019'
 
-filterrows = (df['Date'] > startdate) & (df['Date'] <= enddate)
+    filterrows = (df['Date'] > startdate) & (df['Date'] <= enddate)
 
-df = df.loc[filterrows]
+    df = df.loc[filterrows]
 
-#Drop columns 
-df.drop(columns=['ID'], inplace = True)
-print(type(df))
-
-
-# remove 'b' flags
-df['Tweets'] = df['Tweets'].apply(ast.literal_eval).str.decode("utf-8")
+    #Drop columns 
+    df.drop(columns=['ID'], inplace = True)
+    print(type(df))
 
 
-# remove https links
-df['Tweets'] = df['Tweets'].str.replace(r'http\S+|www.\S+', '', case=False)
+    # remove 'b' flags
+    df['Tweets'] = df['Tweets'].apply(ast.literal_eval).str.decode("utf-8")
 
-# Removal of Null Values
-df['Tweets'].replace('',np.nan,inplace=True)
-df.dropna(axis = 0, how = 'any', inplace = True)
-print(df)
 
-#remove username in Tweets
-df['Tweets'] = df['Tweets'].str.replace(r'@[^\s]+','', case=False)
+    # remove https links
+    df['Tweets'] = df['Tweets'].str.replace(r'http\S+|www.\S+', '', case=False)
 
-# Remove Special Characters 
-df['Tweets'] = df['Tweets'].str.replace(r'[^A-Za-z0-9 ]+', '', case = False)
+    # Removal of Null Values
+    df['Tweets'].replace('',np.nan,inplace=True)
+    df.dropna(axis = 0, how = 'any', inplace = True)
+    print(df)
 
-# Replace empty cells with Nan then Remove Empty Rows
-df['Tweets'].replace('',np.nan,inplace=True)
-df.dropna(axis = 0, how = 'any', inplace = True)
+    #remove username in Tweets
+    df['Tweets'] = df['Tweets'].str.replace(r'@[^\s]+','', case=False)
 
-# Removal of Null Values
-df['Tweets'].replace('',np.nan,inplace=True)
-df['Tweets'].replace(' ',np.nan,inplace=True)
-df['Tweets'].replace('  ',np.nan,inplace=True)
-df['Tweets'].replace('   ',np.nan,inplace=True)
-df['Tweets'].replace('    ',np.nan,inplace=True)
-df.dropna(axis = 0, how = 'any', inplace = True)
-print(df.head(10))
+    # Remove Special Characters 
+    df['Tweets'] = df['Tweets'].str.replace(r'[^A-Za-z0-9 ]+', '', case = False)
 
-#convert datetime to date time
-df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
-df['Date'] = pd.to_datetime(df['Date'])
+    # Replace empty cells with Nan then Remove Empty Rows
+    df['Tweets'].replace('',np.nan,inplace=True)
+    df.dropna(axis = 0, how = 'any', inplace = True)
 
-#Removal of Trailing White Spaces
-dfframe = df['Tweets'].to_frame()
-dflist = df['Tweets'].tolist()
+    # Removal of Null Values
+    df['Tweets'].replace('',np.nan,inplace=True)
+    df['Tweets'].replace(' ',np.nan,inplace=True)
+    df['Tweets'].replace('  ',np.nan,inplace=True)
+    df['Tweets'].replace('   ',np.nan,inplace=True)
+    df['Tweets'].replace('    ',np.nan,inplace=True)
+    df.dropna(axis = 0, how = 'any', inplace = True)
+    print(df.head(10))
 
-dftweet2 = df['Tweets'].str.strip()
-df2 = dftweet2.to_frame()
+    #convert datetime to date time
+    df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
+    df['Date'] = pd.to_datetime(df['Date'])
 
-final = pd.concat([df['User'], df['Date'], df['Tweets']], axis = 1)
+    #Removal of Trailing White Spaces
+    dfframe = df['Tweets'].to_frame()
+    dflist = df['Tweets'].tolist()
 
-# Convert list DataFrame to csv
-pd.DataFrame.from_dict(data = final , orient = 'columns').to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/CSV/Cleanse/' + datefile + 'Cleanse.csv')
+    dftweet2 = df['Tweets'].str.strip()
+    df2 = dftweet2.to_frame()
+
+    final = pd.concat([df['User'], df['Date'], df['Tweets']], axis = 1)
+
+    # Convert list DataFrame to csv
+    pd.DataFrame.from_dict(data = final , orient = 'columns').to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/CSV/Cleanse/' + datefile + 'Cleanse.csv')
+
+UserCleanse()
