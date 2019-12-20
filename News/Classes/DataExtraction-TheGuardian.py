a37d6cfa63465278122c879036e757c9b32726f6
Jurgen Tan <jurgentan@hotmail.com>
2019-12-20 17:27:06 +0800

    Update DataExtraction-TheGuardian.py
    Wrapped the functions within a class and added an __intit__ class function

--- News/Classes/DataExtraction-TheGuardian.py
+++ News/Classes/DataExtraction-TheGuardian.py
@@ -4,62 +4,76 @@ import pandas as pd
 from goose3 import Goose
 import os
 
-
-def NewsExtraction():
-    api_key = '9c31513f-beb6-4aac-86ee-0b1320f2860b'
-
-    url = 'https://content.guardianapis.com/search?'
-
-    query = "Samsung"#str(input("Query: "))
-    fromdate = "2018-07-01"#str(input("From Date (YYYY-MM-DD): "))
-    todate = "2019-11-01"#str(input("To Date (YYYY-MM-DD): "))
-    page = 1 #int(input("Page Number: "))
+class DataExtraction:
+    "This class handles all the extraction for the news articles from The Guardian"
 
     
-    df = pd.DataFrame(columns =('Section', 'Title', 'URL', 'Date', 'Text'))
-    
-    # totalpages = page 
-    # while page <= totalpages:
-
-    parameters = {
-    'q': query,
-    'from-date': fromdate,
-    'to-date': todate,
-    'order-by': 'newest',
-    'page-size': 200, #200 max
-    'page': page,
-    'api-key': api_key
-    }
-
-    print("Current Page Number: " + str(page))
-
-    response = requests.get(url, params=parameters)
-    response_json = response.json()['response']['results']
-    totalpage = response.json()['response']['pages']
-    print(response.json()['response']['total'])
-    print("The total number of pages to be extracted: " + str(totalpage))
+    def __init__(self, api_key, url, query, fromdate, todate, page = 1):
+        self.api_key = api_key
+        self.url = url
+        self.query = query
+        self.fromdate = fromdate
+        self.todate = todate
+        self.page = page
 
 
-    g = Goose()
+    def NewsExtraction(self):
+        # self.api_key = '9c31513f-beb6-4aac-86ee-0b1320f2860b'
 
-    i = 0
-    for article in response_json:
-        i = i + 1
-        section = article['sectionName']
-        title = article['webTitle']
-        url = article['webUrl']
-        date = article['webPublicationDate']
-        text = g.extract(url=url)
-        text = text.cleaned_text
-        df.loc[i] = section, title, url, date, text
-        # page += 1
+        # self.url = 'https://content.guardianapis.com/search?'
 
-    export_csv = input("Do you want to export the data to a csv file? Yes or No? ")
+        # self.query = "Samsung"#str(input("Query: "))
+        # self.fromdate = "2018-07-01"#str(input("From Date (YYYY-MM-DD): "))
+        # self.todate = "2019-11-01"#str(input("To Date (YYYY-MM-DD): "))
+        # self.page = 1 #int(input("Page Number: "))
 
-    if export_csv.upper() =="YES":
-        df.to_csv(parameters["q"] + ".csv", index=False)
-        print(os.getcwd())
-    else: 
-        print(df['Title'])
+        
+        df = pd.DataFrame(columns =('Section', 'Title', 'URL', 'Date', 'Text'))
+        
+        parameters = {
+        'q': self.query,
+        'from-date': self.fromdate,
+        'to-date': self.todate,
+        'order-by': 'newest',
+        'page-size': 200, #200 max
+        'page': self.page,
+        'api-key': self.api_key
+        }
 
-NewsExtraction()
+        print("Current Page Number: " + str(self.page))
+
+        response = requests.get(self.url, params=parameters)
+        response_json = response.json()['response']['results']
+        totalpage = response.json()['response']['pages']
+        print(response.json()['response']['total'])
+        print("The total number of pages to be extracted: " + str(totalpage))
+
+
+        g = Goose()
+
+        i = 0
+        for article in response_json:
+            i = i + 1
+            section = article['sectionName']
+            title = article['webTitle']
+            url = article['webUrl']
+            date = article['webPublicationDate']
+            text = g.extract(url=url)
+            text = text.cleaned_text
+            df.loc[i] = section, title, url, date, text
+            # page += 1
+
+        export_csv = input("Do you want to export the data to a csv file? Yes or No? ")
+
+        if export_csv.upper() =="YES":
+            df.to_csv(parameters["q"] + ".csv", index=False)
+            print(os.getcwd())
+        else: 
+            print(df['Title'])
+extraction = DataExtraction(
+    "9c31513f-beb6-4aac-86ee-0b1320f2860b", 
+    "https://content.guardianapis.com/search?",
+    "Samsung",
+    "2018-07-01",
+    "2019-11-01")
+print(extraction.NewsExtraction())
