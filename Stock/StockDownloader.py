0ee8e5b6bbc6e1f22a2ead4352aaec6a13bf8af0
Jurgen Tan <jurgentan@hotmail.com>
2019-12-24 10:23:30 +0800

    Update StockDownloader.py

--- Stock/StockDownloader.py
+++ Stock/StockDownloader.py
@@ -1,10 +1,16 @@
-# Please make sure that the library has already been downloaded
-# pip install yfinance
 import yfinance as yf
 
-fromdate = input("Please input the start date to be extracted(yyyy-mm-dd): ")
-todate = input("Please input the start date to be extracted(yyyy-mm-dd): ")
-stock = input("Please input which stock you would like to extract in its index format: ")
+def StockDownloader():
+    """ 
+    Please make sure that the library has already been downloaded: pip install yfinance
+    The dates entered have to be in the yyyy-mm-dd format
+    Stock indexs have to be in capitalised alphabet
+    """
+    fromdate = input("Please input the start date to be extracted(yyyy-mm-dd): ")
+    todate = input("Please input the start date to be extracted(yyyy-mm-dd): ")
+    stock = input("Please input which stock you would like to extract in its index format in capital letters: ").upper()
 
-data = yf.download(stock, start = fromdate, end = todate)
-data.to_csv(stock + ".csv")
+    data = yf.download(stock, start = fromdate, end = todate)
+    data.to_csv(stock + ".csv")
+
+StockDownloader()
