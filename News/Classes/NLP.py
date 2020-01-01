ed64078ad92750c26ea204c6a7eb8da39cdec4ac
charmaineneo <56992719+charmaineneo@users.noreply.github.com>
2020-01-01 14:51:09 +0800

    NLP Class

--- News/Classes/NLP.py
+++ News/Classes/NLP.py
@@ -12,75 +12,73 @@ from nltk.corpus import wordnet as wn
 from nltk.corpus import sentiwordnet as swn
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import csv
 
 
 #Reading of .csv file
+class class_NLP:
 
-df = pd.read_csv(r"C:\Users\charmaine\Desktop\YEAR3\FYP\NLP_Class\TheGuardianDataFinal.csv")
+   filename = r"C:\Users\charmaine\Desktop\YEAR3\FYP\FYP\News\Classes\Samsung.csv"
+
+   def __inti__(self):
+      pass
+
+   #Read csv file
+   def read_file(self):
+      print("Reading csv file")
+      self.data_as_csv = pd.read_csv(self.filename)
+      print("There are {} column attributes: {}".format(len(self.data_as_csv.columns), list(self.data_as_csv)))
+      return self.data_as_csv
+      
+   #Tokenization and removal of punctuations
+   def tokenize(self, content):
+      print("Tokenizing content")
+      tokenizer = RegexpTokenizer(r'\w+')
+      self.content = content.apply(lambda x: tokenizer.tokenize(x))
+      print("Content is tokenized")
+      return self.content
+
+   #Removal of Stop Words
+   def stopwords_removal(self):
+      print("Removing stopwords")
+      set(stopwords.words('english'))
+
+      def remove_stopwords(text):
+         self.words = [w for w in text if w not in stopwords.words('english')]
+         return self.words
+
+      self.content = self.content.apply(lambda x: remove_stopwords(x))
+      print("Stopwords are removed")
+      return self.content
+   
+   #Lemmatization
+   def lemmatization(self):
+      print("Lemmatizing content")
+      lemmatizer = WordNetLemmatizer()
+
+      def word_lemmatizer(text):
+         self.lem_text = [lemmatizer.lemmatize(i, pos="a") for i in text]
+         self.lem_text = [lemmatizer.lemmatize(i, pos="s") for i in text]
+         self.lem_text = [lemmatizer.lemmatize(i, pos="v") for i in text]
+         self.lem_text = [lemmatizer.lemmatize(i, pos="r") for i in text]
+         self.lem_text = [lemmatizer.lemmatize(i, pos="n") for i in text]
+         return self.lem_text
+
+      self.content = self.content.apply(lambda x: word_lemmatizer(x))
+      print("Content is lemmatized")
+      return self.content
+
+   #Part of Speech tagging
+   def nltk_pos_tag(self):
+      print("Tagging words")
+      self.content = [nltk.pos_tag(i) for i in self.content]
+      print("Words are tagged")
+      return self.content
+
+News_NLP = class_NLP()
+df = News_NLP.read_file()
 content = df['Text']
-assert content.isnull().count() == len(content)
-
-
-#Word count of each article before removal of stop words
-
-def word_count(content):
-    count = [len(content[i]) for i in range(len(content))]
-    return count
-
-
-#Tokenization and removal of punctuations
-
-def tokenize(content):
-    tokenizer = RegexpTokenizer(r'\w+')
-    content = content.apply(lambda x: tokenizer.tokenize(x))
-    return content
-tokenize(content)
-print(content)
-
-#Removal of Stop Words
-
-def remove_stopwords(text):
-    set(stopwords.words('english'))
-    words = [w for w in text if w not in stopwords.words('english')]
-    return words
-remove_stopwords(content)
-
-#Lemmatization
-
-def word_lemmatizer(text):
-    lemmatizer = WordNetLemmatizer()
-
-    lem_text = [lemmatizer.lemmatize(i, pos="a") for i in text]
-    lem_text = [lemmatizer.lemmatize(i, pos="s") for i in text]
-    lem_text = [lemmatizer.lemmatize(i, pos="v") for i in text]
-    lem_text = [lemmatizer.lemmatize(i, pos="r") for i in text]
-    lem_text = [lemmatizer.lemmatize(i, pos="n") for i in text]
-    return lem_text
-
-word_lemmatizer(content)
-
-print(content[0])
-content_freq = content
-
-
-#Word count of each article after removal of stop words
-
-countaft = [len(content[i]) for i in range(len(content))]
-print(countaft)
-
-
-#Part of Speech tagging
-
-def nltk_pos_tag(content):
-    content_tag = [nltk.pos_tag(i) for i in content]
-    return content_tag
-
-#Difference of word count before and after stop word removal in percentage, rounded off to 1dp
-
-count_diff = []
-for i in range(len(count)):
-    count_diff.append(round(((count[i] - countaft[i])/count[i])*100, 1))
-print(count_diff)
-
-
-df.to_csv("Formatted_Data_Sentiment.csv", index = False)
+News_Tokenizing = News_NLP.tokenize(content)
+News_RemoveWords = News_NLP.stopwords_removal()
+News_Lemmatizing = News_NLP.lemmatization()
+News_POStag = News_NLP.nltk_pos_tag()
