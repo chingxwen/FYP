4f4ded9dbe38ee24784d05a0a47c2c9420b3fbfe
chingxwen <52928218+chingxwen@users.noreply.github.com>
2020-01-13 22:12:16 +0800

    Translate

--- Social Media/reddit/Data/Cleanse/pushshiftcleansing.py
+++ Social Media/reddit/Data/Cleanse/pushshiftcleansing.py
@@ -8,7 +8,8 @@ import re
 
 
 #Reading of data file
-df = pd.read_csv('C:/Users/jiajie/Desktop/FYP/reddit/Data/Pulled/pushshiftsamsungsubmission2018August.csv')
+# df = pd.read_csv('C:/Users/jiajie/Desktop/FYP/reddit/Data/Pulled/pushshiftsamsungsubmission2018August.csv')
+df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Pulled/pushshiftsamsungsubmission2018August.csv')
 
 #Removal of Links
 df['title'] = df['title'].str.replace(r'http\S+|www.\S+', '', case=False)
@@ -24,13 +25,26 @@ print('Special Characters Removed')
 titlelist = df['title'].tolist()
 words = set(nltk.corpus.words.words())
 
-englishonly  =" ".join(w for w in nltk.wordpunct_tokenize(titlelist[4]) \
-         if w.lower() in words or not w.isalpha())
+translate = []
 
-print('success')
-print(titlelist[4])
-print(englishonly)
+for i in range(len(titlelist)):
 
+    englishonly  = " ".join(w for w in nltk.wordpunct_tokenize(titlelist[i]) \
+            if w.lower() in words or not w.isalpha())
+    
+    translate.append(englishonly)
+    
+    print('success')
+
+
+print(translate)
+translateText = pd.DataFrame(translate)
+translateTime = df['timestamp']
+translateReddit = df['subreddit']
+
+df = pd.concat([translateReddit, translateText, translateTime] , axis = 1)
 # output to csv
-df.to_csv('C:/Users/jiajie/Desktop/FYP/reddit/Data/Pulled/Cleansed/pushshiftsamsungsubmission2018AugustCleansed.csv', sep=',', index = False, header = True)
+# df.to_csv('C:/Users/jiajie/Desktop/FYP/reddit/Data/Pulled/Cleansed/pushshiftsamsungsubmission2018AugustCleansed.csv', sep=',', index = False, header = True)
+df.to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Pulled/Cleansed/pushshiftsamsungsubmission2018AugustCleansed.csv', sep=',', index = False, header = True)
+
 print('written!')
