b686f1a47f086dd3b51af4c93db3a5d3a8414f6e
chingxwen <52928218+chingxwen@users.noreply.github.com>
2020-01-21 10:09:07 +0800

    updated reddit

--- Social Media/reddit/Data/Pulled/pushshiftcomments.py
+++ Social Media/reddit/Data/Pulled/pushshiftcomments.py
@@ -1,3 +1,4 @@
+
 from pushshift_py import PushshiftAPI
 import praw
 import datetime as dt
@@ -17,71 +18,54 @@ def get_redditcomments():
 
     start_epoch=int(dt.datetime(2019,10,1).timestamp())
 
-    gen = api.search_submissions(after = start_epoch,
+    gen = api.search_comments(q = 'samsung',
                                 subreddit = 'samsung',
-                                q = 'samsung',
-                                filter = ['subreddit', 'created','title','id'],
-                                limit = 1000)
+                                after = start_epoch,
+                                filter = ['created','body','id'],
+                                size = 1)
     results = list(gen)
-    # print(results)
-
-    #Getting post comments
+    print('Query Done!')
 
     data = pd.DataFrame(results)
-
-    iddata = data[['id']]
-    iddata = iddata.values.tolist()
-
-    for i in range(len(iddata)):
-        def listTostring(s):
-            str1 = ''
-            return(str1.join(s))
-        print(listTostring(iddata[i]))
-
-    for i in range(len(iddata)):
-        try:
-            x = reddit.submission(listTostring(iddata[i]))
-            for comment in x.comments:
-                for reply in comment.replies:
-                    length  = len(iddata)
-                    cData = []
-                    cData.append(reply.body)
-                    print('Comment')
-                    print(reply.body)
-                    cTime = []
-                    cTime.append(reply.created_utc)
-                    print('Time')
-                    print(reply.created_utc)
-                    print('\n')
-                    print(i, 'out of', length)    
-        except Exception as e:
-            print(type(e))
-            print(e)
-    
-
-    data = pd.DataFrame(results)
-    print(data.head(5))
-    #Converting datetime column from UNIX to normal
-    # def get_date(created):
-    #     return dt.datetime.fromtimestamp(created)
-    # _timestamp = data["created"].apply(get_date)
-    # data = data.assign(timestamp = _timestamp)
-
-    #drop columns
-    data = data.drop('created_utc', axis = 1)
-    data = data.drop('id', axis = 1)
-    data = data.drop('subreddit', axis = 1)
-    data = data.drop('title', axis=1)
-    data = data.drop('d_', axis=1)
-    data = data.drop('created', axis = 1)
-
-    #Assigning comments
-    data = data.assign(comments = cData)
-    data = data.assign(created = cTime)
-
+    print(data.head(5))    
     #Write data to csv
-    # data.to_csv('C:/Users/jiajie/Desktop/FYP/reddit/Data/pushshiftsamsungsubmission2019October.csv', index=False)
-    print('Written!')
-    print(data.head(5))
+    data.to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/pushshiftsamsungsubmission2019October.csv')
+#     print('Written!')
+#     print(cData)
 
 get_redditcomments()
+
+
+
+# import praw
+# import datetime as dt
+# import pandas as pd
+# from praw.models import MoreComments
+# from psaw import PushshiftAPI
+
+# def get_redditcomments():
+#     api = PushshiftAPI()
+#     reddit = praw.Reddit(
+#         client_id = 'ft1YI89jxATR_g', \
+#         client_secret = '31k1f4ORFpgtQlZ-h2QLO9qxCPc', \
+#         user_agent = 'scrubmasterAPI', \
+#         username = 'pythonscrubSP', \
+#         password = 'scrubmaster54321'
+#     )
+
+#     start_epoch=int(dt.datetime(2017, 1, 1).timestamp())
+
+#     gen = api.search_comments(q = 'samsung',
+#                               id = 'dbewaq',
+#                               after = start_epoch,
+#                               subreddit = 'samsung',
+#                               filter=['title', 'subreddit', 'body'],
+#                               limit = 10)
+    
+#     results = list(gen)
+
+    
+#     data = pd.DataFrame(results)
+#     data.to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/commentsnew.csv')
+#     print('Done!')
+# get_redditcomments()
