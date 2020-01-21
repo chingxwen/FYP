a382ef1d852dcd997e898c079b656895d1a0f56b
chingxwen <52928218+chingxwen@users.noreply.github.com>
2020-01-21 13:38:19 +0800

    reddit cleansing

--- Social Media/reddit/Cleanse/pushshiftcleanse.py
+++ Social Media/reddit/Cleanse/pushshiftcleanse.py
@@ -1,10 +1,73 @@
 import pandas as pd 
 import csv
 import re
+import datetime as dt
+import numpy as np
 
-df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/pushshiftandroid.csv')
+df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/pushshiftsamsungsubmission2019October.csv')
+
+
+#Converting datetime column from UNIX to normal
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+
+_timestamp = df["created"].apply(get_date)
+df = df.assign(timestamp = _timestamp)
+
 
 #remove duplicate rows
-df.drop_duplicates(subset = 'title' ,keep = 'first', inplace = True)
+df.drop(columns=['id'], inplace = True)
+df.drop(columns=['d_'], inplace = True)
+df.drop(columns=['created_utc'], inplace = True)
+df.drop(columns=['created'], inplace = True)
+df.drop(columns=['Unnamed: 0'], inplace = True)
+print(df.columns.values)
 
-df.to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Cleanse/pushshiftandroidcleanse.csv')
+
+#remove links 
+df['body'] = df['body'].str.replace(r'http\S+|www.\S+', '', case=False)
+
+#convert timestamp to data time object 
+
+df['timestamp'] = pd.to_datetime(df['timestamp'] ,format='%YY%mm%dd').dt.date
+
+#drop null values
+df['body'].replace('',np.nan,inplace=True)
+df.dropna(axis = 0, how = 'any', inplace = True)
+
+#keyword search to remove automated messagede from reddit 
+
+dict = {"*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](/message/compose/?to=/r/samsung) if you have any questions or concerns.*"
+}
+
+bodylist = df['body'].tolist()
+
+bodycontent = df['body']
+datecontent = df['timestamp']
+
+releventbody = []
+releventdate = []
+count = 0
+
+for words in dict:
+    for x in range(len(bodycontent)):
+        if (words in bodycontent.iloc[x]) == False:
+                print(words)
+                count += 1 
+
+                print(bodycontent.iloc[x])
+
+                releventbody.append(bodycontent.iloc[x])
+                releventdate.append(datecontent.iloc[x])
+
+        else:
+            pass
+
+dfbody = pd.DataFrame(releventbody, columns = ['body'])
+dfDate = pd.DataFrame(releventdate, columns = ['timestamp'])
+
+df = pd.concat([dfbody, dfDate], axis = 1)
+
+print(df)
+
+pd.DataFrame.from_dict(data = df , orient = 'columns').to_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Cleanse/pushshiftandroidcleanse.csv')
--- Social Media/reddit/Data/Pulled/pushshiftcomments.py
+++ Social Media/reddit/Data/Pulled/pushshiftcomments.py
@@ -20,15 +20,9 @@ def get_redditcomments():
 
     gen = api.search_comments(q = 'samsung',
                                 subreddit = 'samsung',
-<<<<<<< HEAD
                                 after = start_epoch,
                                 filter = ['created','body','id'],
                                 size = 1)
-=======
-                                q = 'samsung',
-                                filter = ['subreddit', 'created','title','id'],
-                                limit = 5)
->>>>>>> 67674830934446bc58ae4534df6d1803a4fbbf79
     results = list(gen)
     print('Query Done!')
 
@@ -41,7 +35,6 @@ def get_redditcomments():
 
 get_redditcomments()
 
-<<<<<<< HEAD
 
 
 # import praw
@@ -70,48 +63,6 @@ get_redditcomments()
 #                               limit = 10)
     
 #     results = list(gen)
-=======
-    for i in range(len(iddata)):
-        def listTostring(s):
-            str1 = ''
-            return(str1.join(s))
-        print(listTostring(iddata[i]))
-    cData = []
-    for i in range(len(iddata)):
-        try:
-            x = reddit.submission(listTostring(iddata[i]))
-            for comment in x.comments:
-                comment = comment.body
-                cData.append(comment)
-                # cData.append(comment)
-                # print('appended')
-                # for reply in comment.replies:
-                #     length = len(iddata)
-                #     reply = reply.body
-                #     cData.append(reply)
-                #     # print('Comment Appended')
-                #     # replyt = str(reply.created_utc)
-                #     # cTime.append(replyt)
-                #     # print('Time Appeneded')
-                #     # print('\n')
-                #     print(i, 'out of', length)
-        except Exception as e:
-            print(type(e))
-            print(e)
-    
-
-    #Change to series for concat
-    cData = pd.DataFrame(cData)
-    # cTime = pd.DataFrame(cTime)
-    # frames = [cData, cTime]
-    # comdata = pd.concat(frames)
-    # print(comdata)
-
-    #Write data to csv
-    cData.to_csv('C:/Users/jiajie25/Documents/GitHub/FYP/Social Media/reddit/Data/Pulled/comments/comments.csv', index=False)
-    print('Written!')
-    print(cData)
->>>>>>> 67674830934446bc58ae4534df6d1803a4fbbf79
 
     
 #     data = pd.DataFrame(results)
