a4ba36692d18487cf431dc882e4ac6cfe0615fca
jiajieSP <56992742+jiajieSP@users.noreply.github.com>
2020-01-22 23:38:21 +0800

    Reddit Self Text Cleanse

--- Social Media/reddit/Cleanse/RedditSelfTextCleanse.py
+++ Social Media/reddit/Cleanse/RedditSelfTextCleanse.py
@@ -5,17 +5,43 @@ import datetime as dt
 import numpy as np
 
 datafile = input('whcih file would you like to clense?')
-df = pd.read_csv('C:/FYP/Social Media/reddit/Data/Pulled/raw/' + datafile + '.csv', )
+df = pd.read_csv('C:/FYP/Social Media/reddit/Data/Pulled/raw/' + datafile + '.csv')
+
+df['selftext'].astype('object').dtypes
 
 df.sort_values("id", inplace = True)
 df.drop_duplicates(subset="id", keep="first", inplace=True)
+print('duplicates removed')
+
+#remove duplicate rows
+df.drop(columns=['id'], inplace = True)
+df.drop(columns=['subreddit'], inplace=True)
+print('unwanted columns removed')
+
+#remove links 
+df['selftext'] = df['selftext'].str.replace(r'http\S+|www.\S+', '', case=False)
+print('links removed')
+
+#remove spaces
+dflist = df['selftext'].tolist()
+dfnewlist = []
+
+for x in range(len(dflist)):
+
+    dflist[x] = " ".join(dflist[x].split())
+    dflist[x] = dflist[x].replace('\r', '').replace('\n', '')
+    dfnewlist.append(dflist[x])
+    # print(dflist[x])
+
+#convert to dataframe again
+df['selftext'] = pd.DataFrame(dfnewlist)
+df = pd.concat([df['selftext'],df['timestamp']], axis = 1)
+
+#dropping NaN
+df = df.dropna()
+print('NaN dropped')
 print(df)
-#Converting datetime column from UNIX to normal
-def get_date(created):
-    return dt.datetime.fromtimestamp(created)
 
-_timestamp = df["created"].apply(get_date)
-df = df.assign(timestamp = _timestamp)
-
-print(df)
-
+#Write out
+pd.DataFrame.from_dict(data = df , orient = 'columns').to_csv('C:/FYP/Social Media/reddit/Data/Cleanse/Raw/' + datafile + 'Cleanse.csv')
+print('written!')
