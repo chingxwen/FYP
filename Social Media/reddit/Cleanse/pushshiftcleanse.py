5c51615a92c54e0c62d1c925cd7e1a8790e913eb
chingxwen <52928218+chingxwen@users.noreply.github.com>
2020-01-22 14:59:45 +0800

    Cleansed Comments

--- Social Media/reddit/Cleanse/pushshiftcleanse.py
+++ Social Media/reddit/Cleanse/pushshiftcleanse.py
@@ -5,7 +5,11 @@ import datetime as dt
 import numpy as np
 
 datafile = input('whcih file would you like to clense?')
-df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Pulled/comments/' + datafile + '.csv')
+df = pd.read_csv('C:/Users/User/Desktop/FYP/FYP/Social Media/reddit/Data/Pulled/comments/' + datafile + '.csv', )
+
+df['body'].astype('object').dtypes
+print(df.dtypes.value_counts())
+
 
 
 #Converting datetime column from UNIX to normal
@@ -31,10 +35,6 @@ df['body'] = df['body'].str.replace(r'http\S+|www.\S+', '', case=False)
 
 df['timestamp'] = pd.to_datetime(df['timestamp'] ,format='%YY%mm%dd').dt.date
 
-#drop null values
-df['body'].replace('',np.nan,inplace=True)
-df.dropna(axis = 0, how = 'any', inplace = True)
-
 dflist = df['body'].tolist()
 dfnewlist = []
 #removel spaces
@@ -44,7 +44,7 @@ for x in range(len(dflist)):
     dflist[x] = " ".join(dflist[x].split())
     dflist[x] = dflist[x].replace('\r', '').replace('\n', '')
     dfnewlist.append(dflist[x])
-    print(dflist[x])
+    # print(dflist[x])
 
 #convert to dataframe again
 
@@ -59,7 +59,7 @@ dict = {"*I am a bot, and this action was performed automatically. Please [conta
 
 bodylist = df['body'].tolist()
 
-bodycontent = df['body']
+bodycontent = df['body'].astype(str)
 datecontent = df['timestamp']
 
 releventbody = []
@@ -68,6 +68,7 @@ count = 0
 
 for words in dict:
     for x in range(len(bodycontent)):
+        print(type(bodycontent.iloc[x]))
         if (words in bodycontent.iloc[x]) == False:
                 count += 1 
 
@@ -81,6 +82,14 @@ dfbody = pd.DataFrame(releventbody, columns = ['body'])
 dfDate = pd.DataFrame(releventdate, columns = ['timestamp'])
 
 df = pd.concat([dfbody, dfDate], axis = 1)
+
+#drop null values
+df['body'].replace('',np.nan,inplace=True)
+df.dropna(axis = 0, how = 'any', inplace = True) 
+
+#drop special characters
+
+df['body'].str.replace("[","")
 
 print(df)
 
